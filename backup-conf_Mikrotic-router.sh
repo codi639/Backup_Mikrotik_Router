c2eeabf6d7be7bdb0cfad1f7d8dc0bb40f56f6dd
Jean-Lou Gilbertas <113203030+codi639@users.noreply.github.com>
2023-10-24 10:44:31 +0200

    Add files via upload

---
+++ backup-conf_Mikrotic-router.sh
@@ -0,0 +1,27 @@
+#/bin/bash
+
+
+Router_IP="192.168.1.1"
+Router_Username="admin"
+#Router_Password="admin"
+
+File_Name=$(date +'%d-%m-%Y')
+
+local_path_to_save="/home/jean-lou/Documents/Entrainements"
+
+SSH_Export_Conf="/export file=Configuration"
+SSH_Export_Backup="/system backup save name=Backup dont-encrypt=yes password=admin encryption=aes-sha256"
+
+
+#sshpass -p "$Router_Password" 
+ssh -i /home/jean-lou/.ssh/id_rsa.pub "$Router_Username"@"$Router_IP" "$SSH_Export_Conf"
+#sshpass -p "$Router_Password" 
+ssh -i /home/jean-lou/.ssh/id_rsa.pub "$Router_Username"@"$Router_IP" "$SSH_Export_Backup"
+
+scp -i /home/jean-lou/.ssh/id_rsa.pub "$Router_Username"@"$Router_IP":Configuration.rsc "$local_path_to_save/$File_Name-Conf.src"
+scp -i /home/jean-lou/.ssh/id_rsa.pub "$Router_Username"@"$Router_IP":Backup.backup "$local_path_to_save/$File_Name-Back.backup"
+
+
+
+
+
